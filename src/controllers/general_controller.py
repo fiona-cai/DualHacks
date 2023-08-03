1f8f74d453cb8327354898fc24104600487a0a30
VictorK <victorkwong128@gmail.com>
2023-08-03 19:01:05 -0400

    2023-08-03
    Changes made to routes

--- src/controllers/general_controller.py
+++ src/controllers/general_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 
 general_blueprint = Blueprint("general", __name__)
 
@@ -13,12 +13,13 @@ def home():
 def login():
     if request.methods == "POST":
         user = request.form[""]
+        return redirect(url_for("user", usr=user))
     else:
         return render_template("login.html")
 
 
 @general_blueprint.route("/user/<username>/profile", methods=["GET"])
-def user():
+def profile():
     return render_template("profile.html")
 
 
@@ -27,4 +28,7 @@ def signup():
     return render_template("signup.html")
 
 
+@general_blueprint.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
