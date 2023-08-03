4934e36294f0019b63633c7a46db625a96534baa
VictorK <victorkwong128@gmail.com>
2023-08-03 17:51:04 -0400

    2023-08-3
    Added some html files
    Worked on routes for the app

--- src/controllers/general_controller.py
+++ src/controllers/general_controller.py
@@ -4,6 +4,21 @@ general_blueprint = Blueprint("general", __name__)
 
 
 @general_blueprint.route("/")
-@general_blueprint.route("/home")
+@general_blueprint.route("/home/")
 def home():
     return render_template("home.html")
+
+@general_blueprint.route("/login/", methods=["POST","GET"])
+def login():
+    return render_template("login.html")
+
+@general_blueprint.route("/<usr>")
+def user(usr):
+    return f'<h1>{usr}</h1>'
+
+@general_blueprint.route("/signup/")
+def signup():
+    return render_template("signup.html")
+
+
+
