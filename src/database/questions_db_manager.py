97d6b7b046fe82c63d7b9a3562c8c74cea9a87d0
Mani Fatemi <70338931+Ma1305@users.noreply.github.com>
2023-08-04 00:21:13 -0400

    databse structure

---
+++ src/database/questions_db_manager.py
@@ -0,0 +1,49 @@
+from models.question import Question
+
+
+def get_question_by_id(question_id):
+    """
+    returns None if question not found
+    :param question_id:
+    :return:
+    """
+    pass
+
+
+def get_questions_by_subject(subject):
+    """
+    returns empty array if no question with specified subject found
+    :param subject:
+    :return:
+    """
+    pass
+
+
+def add_question(question):
+    """
+    False for failure
+    True for successful operation
+    :param question:
+    :return:
+    """
+    pass
+
+
+def delete_question(question):
+    """
+    False for failure
+    True for successful operation
+    :param question:
+    :return:
+    """
+    pass
+
+
+def delete_question_by_id(question_id):
+    """
+    False for failure
+    True for successful operation
+    :param question_id:
+    :return:
+    """
+    pass
---
+++ src/database/users_db_manager.py
@@ -0,0 +1,53 @@
+from models.user import User
+
+
+def get_user_by_username(username):
+    """
+    returns None if user not found
+    :param username:
+    :return: User object
+    """
+    pass
+
+
+def get_users_with_name(name):
+    """
+    returns empty array if user with name not found
+    :param name:
+    :return: array of User objects
+    """
+    pass
+
+
+def add_user(user):
+    """
+    returns False if operation unsuccessful
+    returns True if operation is successful
+    :param user:
+    :return: boolean
+    """
+    pass
+
+
+def delete_user(user):
+    """
+    returns False if operation unsuccessful
+    returns True if operation is successful
+    :param user:
+    :return: boolean
+    """
+    pass
+
+
+def update_user(user):
+    """
+    returns False if operation unsuccessful
+    returns True if operation is successful
+    :param user:
+    :return: boolean
+    """
+    pass
+
+
+def count_users():
+    pass
