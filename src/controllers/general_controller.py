32878e7af6313bb5dc2ef039a3e3d7c0cfbef6fb
Mani Fatemi <70338931+Ma1305@users.noreply.github.com>
2023-08-05 01:11:26 -0400

    added some more clarifications and comments

--- src/controllers/general_controller.py
+++ src/controllers/general_controller.py
@@ -22,15 +22,18 @@ def login():
 
     # get the user with username, if does not exist return back to login.html with an error message
     # check password, if wrong return back to login.html with an error message
-    checkUser = users_db.get_user_by_username(request.form["username"])
-    checkPassword = users_db.get_user_by_username(request.form["password"])
-    if checkUser == request.form["username"] and checkPassword == request.form["password"]:
-        return redirect("home.html")
+    user = users_db.get_user_by_username(request.form["username"])  # get_user_by_username will not check anything it will return a user object
+    # you are garanteed that the returned user will have that username so you don't need to check it (its called get by username)
 
-    elif checkUser == None or checkPassword == None:
-        return redirect("login.html")
+    if user is None: # == None would do the same thing
+        pass # change this rendering login but with the "username does not exist message"
+
+    if user.password != request.form["password"]:
+        # add the "password is not correct message
+        return redirect("login.html")  # don't do redirect here it is the url we are on
         # add error message with flask flashes
-    
+
+    # we get to this point of code if the username exists and if the password is correct
     
     # basically setting cookies
     session["username"] = username
