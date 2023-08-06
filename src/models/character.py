1ac09bfcdb3ba7471e26ef3e093065c99a53efba
Mani Fatemi <70338931+Ma1305@users.noreply.github.com>
2023-08-06 16:07:12 -0400

    modifying the models

--- src/models/character.py
+++ src/models/character.py
@@ -3,11 +3,12 @@ from models.power import Power
 
 
 class Character(BaseModel):
-    def __init__(self, name="", powers=[], images_bucket_name="", health=None):
+    def __init__(self, name="", powers=[], images_bucket_name="", health=None, subject=""):
         self.name = name
         self.powers = powers
         self.images_buckets_name = images_bucket_name
         self.health = health
+        self.subject = subject
 
     def setup_powers(self):
         self.powers = [Power(**power) for power in self.powers]
--- src/models/power.py
+++ src/models/power.py
@@ -2,7 +2,7 @@ from models.base_model import BaseModel
 
 
 class Power(BaseModel):
-    def __init__(self, subject="", damage=0, difficulty=0):
-        self.subject = subject
+    def __init__(self, damage=0, difficulty=0, name=""):
         self.damage = damage
         self.difficulty = difficulty
+        self.name = name
