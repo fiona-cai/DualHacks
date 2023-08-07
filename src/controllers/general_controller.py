bbc31b72870b0a67e8a144d8d504d7772355c2a2
Mani Fatemi <70338931+Ma1305@users.noreply.github.com>
2023-08-07 10:27:05 -0400

    setting up some of the email use cases

--- src/controllers/general_controller.py
+++ src/controllers/general_controller.py
@@ -1,7 +1,12 @@
 from flask import Flask, Blueprint, render_template, request, redirect, url_for, session, flash
+import jwt
+from datetime import datetime
+import sys
 
 import database.users_db_manager as users_db
+from helpers import create_jwt, send_email
 from models.user import User
+from settings import MAIL_DEFAULT_SENDER, SECRET_KEY
 
 general_blueprint = Blueprint("general", __name__)
 
@@ -61,8 +66,35 @@ def signup():
     user = User(request.form["username"], request.form["name"], request.form["lastname"],
                 request.form["email"], request.form["password"])
     users_db.add_user(user)
-    # send an email
-    # get their email
+
+    token = create_jwt({'email': user.email, 'username': user.username}, SECRET_KEY)
+    text = render_template('confirmation_email.html', username=user.username, token=token)
+
+    send_email('Account Confirmation', MAIL_DEFAULT_SENDER, [user.email], text)
 
     flash("A confirmation email has been sent, please check your inbox.", "info")
+    return redirect("/login")
+
+
+@general_blueprint.route('/confirmregister/<token>')
+def confirm_register(token):
+    try:
+        token = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
+    except Exception as e:
+        sys.stderr.write(str(e))
+        token = 0
+    if not token:
+        flash("Email verification link invalid", "warning")
+        return redirect("/register")
+
+    user = users_db.get_user_by_username(token['username'])
+
+    if datetime.strptime(token["expiration"], "%Y-%m-%dT%H:%M:%S.%f") < datetime.utcnow():
+        users_db.delete_user(user)
+        flash("Email verification link expired. Please register again using the same email.", "warning")
+        return redirect("/register")
+
+    user.verified = True
+    users_db.update_user(user)
+
     return redirect("/login")
--- src/models/user.py
+++ src/models/user.py
@@ -2,9 +2,10 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    def __init__(self, username="", name="", lastname="", email="", password=""):
+    def __init__(self, username="", name="", lastname="", email="", password="", verified=False):
         self.username = username
         self.name = name
         self.lastname = lastname
         self.email = email
         self.password = password
+        self.verified = verified
